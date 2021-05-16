808b525bd573c662e3544f5b0713e6617ad02a18
SUSH <849806+sushat4692@users.noreply.github.com>
2021-05-17 00:49:01 +0800

    Update Aside.tsx
    * Removed facebook link

--- src/components/SideBar/Aside.tsx
+++ src/components/SideBar/Aside.tsx
@@ -6,8 +6,6 @@ const Aside: React.FC = () => {
     <dl>
       <dt><a href="https://twitter.com/sushat4692" target="_blank" rel="noopener noreferrer">Twitter</a></dt>
       <dd>sushat4692</dd>
-      <dt><a href="http://www.facebook.com/sushat4692" target="_blank" rel="noopener noreferrer">Facebook</a></dt>
-      <dd>sushat4692</dd>
       <dt><a href="http://www.flickr.com/photos/sushat4692/" target="_blank" rel="noopener noreferrer">Flickr</a></dt>
       <dd>sushat4692</dd>
       <dt><a href="https://github.com/sushat4692" target="_blank" rel="noopener noreferrer">GitHub</a></dt>
